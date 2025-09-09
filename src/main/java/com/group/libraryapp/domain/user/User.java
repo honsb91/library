b9b4071839b54a772b1d483d7d7807b97ea491bc
honsb91 <114128759+honsb91@users.noreply.github.com>
2025-09-10 05:43:49 +0900

    ..

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,9 +1,6 @@
 package com.group.libraryapp.domain.user;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class User {
@@ -12,8 +9,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id = null;
 
+    @Column(nullable = false, length = 20) // name varchar(20)
     private String name;
     private Integer age;
+
+    protected User(){
+
+    }
 
     public User(String name,Integer age) {
         if(name == null || name.isBlank()){
@@ -23,11 +25,20 @@ public class User {
         this.age = age;
     }
 
+    public Long getId(){
+        return id;
+    }
+
     public String getName() {
         return name;
     }
 
     public Integer getAge() {
         return age;
+    }
+
+
+    public void updateName(String name){
+        this.name = name;
     }
 }
