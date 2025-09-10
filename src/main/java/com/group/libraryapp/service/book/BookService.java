dd9e83fa20e0909a9fdb043c5ec252f84d3fd7f5
honsb91 <114128759+honsb91@users.noreply.github.com>
2025-09-11 05:12:53 +0900

    ..

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -1,13 +1,53 @@
 package com.group.libraryapp.service.book;
 
-import com.group.libraryapp.repository.book.BookMemoryRepository;
-import com.group.libraryapp.repository.book.BookRepository;
+import com.group.libraryapp.domain.book.Book;
+import com.group.libraryapp.domain.book.BookRepository;
+import com.group.libraryapp.domain.user.User;
+import com.group.libraryapp.domain.user.UserRepository;
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository;
+import com.group.libraryapp.dto.book.request.BookCreateRequest;
+import com.group.libraryapp.dto.book.request.BookLoanRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
+@Service
 public class BookService {
 
-    private final BookRepository bookRepository = new BookMemoryRepository();
+    private final BookRepository bookRepository;
+    private final UserLoanHistoryRepository userLoanHistoryRepository;
+    private final UserRepository userRepository;
 
-    public void saveBook(){
-        bookRepository.saveBook();
+    public BookService(BookRepository bookRepository, UserLoanHistoryRepository userLoanHistoryRepository,
+                       UserRepository userRepository) {
+        this.bookRepository = bookRepository;
+        this.userLoanHistoryRepository = userLoanHistoryRepository;
+        this.userRepository = userRepository;
+    }
+
+    @Transactional
+    public void saveBook(@RequestBody BookCreateRequest request){
+        bookRepository.save(new Book(request.getName()));
+    }
+
+    @Transactional
+    public void loanBook(BookLoanRequest request) {
+        // 책 정보가져오기
+         Book book = bookRepository.findByName(request.getBookName())
+                .orElseThrow(IllegalArgumentException::new);
+
+         // 대출기록 정보를 확인하여 대출중인지 확인
+         // 만약에 확인했는데 , 대출중이라면 예외발생
+         if(userLoanHistoryRepository.existsByBookNameAndIsReturn(book.getName(),false)){
+             throw new IllegalArgumentException("이미 대출된 책입니다.");
+         }
+
+         // 회원 정보 가져오기
+        User user = userRepository.findByName(request.getUserName())
+                .orElseThrow(IllegalArgumentException::new);
+
+         // 회원 정보와 책 정보를 기반으로 UserLoanHistory를 저장
+        userLoanHistoryRepository.save(new UserLoanHistory(user.getId(),book.getName(),false));
     }
 }
--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -6,6 +6,7 @@ import com.group.libraryapp.dto.user.request.UserCreateRequest;
 import com.group.libraryapp.dto.user.request.UserUpdateRequest;
 import com.group.libraryapp.dto.user.response.UserResponse;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -15,28 +16,36 @@ public class UserServiceV2 {
 
     private final UserRepository userRepository;
 
+    //기본생성자
     public UserServiceV2(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
+    //회원등록
+    @Transactional
     public void saveUser(UserCreateRequest request){
      User user = userRepository.save(new User(request.getName(), request.getAge()));
     }
 
+    //회원목록
+    @Transactional(readOnly = true)
     public List<UserResponse> getUsers(){
         return userRepository.findAll().stream()
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
 
+    //회원수정
+    @Transactional
     public void updateUser(UserUpdateRequest request){
         User user = userRepository.findById(request.getId())
                 .orElseThrow(IllegalArgumentException::new);
 
         user.updateName(request.getName());
-        userRepository.save(user);
     }
 
+    //회원삭제
+    @Transactional
     public void deleteUser(String name){
         User user = userRepository.findByName(name)
                 .orElseThrow(IllegalArgumentException::new);
